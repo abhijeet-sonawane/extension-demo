3424bf21b35d24b3e46ffa7f60c28f382783de15
abhijeet <abhijeet.sonawane@raweng.com>
2018-05-23 22:30:04 -0700

    Formats the code

--- server/routes.js
+++ server/routes.js
@@ -1,23 +1,23 @@
 module.exports = {
 	// hello_world is the name of function in this code block
-	"/functions/hello_world" : {
-		GET : function(req, res) {
+	"/functions/hello_world": {
+		GET: function (req, res) {
 			// Returns sample response "Hello World..!!"
 			return this.resSuccess(req, res, "Hello World.. Everyone!!!")
 		}
 	},
-	"/classes/person/objects":{
+	"/classes/person/objects": {
 		POST: {
-			_pre:function(req, res){
-			   if(req.bobjekt.get("age") < 18){
-				return this.resError(req, res, {
-					age: "should be greater than or equal to 18"
-				})
-			   } else {
-				   return this.resSuccess(req, res)
-			   }
+			_pre: function (req, res) {
+				if (req.bobjekt.get("age") < 18) {
+					return this.resError(req, res, {
+						age: "should be greater than or equal to 18"
+					})
+				} else {
+					return this.resSuccess(req, res)
+				}
 			},
-			_post: function(req, res){
+			_post: function (req, res) {
 				req.logger.log("Object saved successfully")
 				req.bobjekt["fullname"] = req.bobjekt.firstname + " " + req.bobjekt.lastname
 				return this.resSuccess(req, res)
